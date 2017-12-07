3af3d2bbe9838debf16579774bf1abd37aeca116
halumz <mashuksadman@gmail.com>
2017-12-08 02:55:54 +0600

    fork me added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import Fork from './components/fork';
 import Gallary from './containers/gallary';
 import SelectedImage from './containers/selectedImage';
 
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <div>
+          <Fork />
           <Gallary />
           <SelectedImage />
         </div>
